898ef44e7ded5cca6c678b46ed284ee7a6eed414
kamildike <kamil.dike.96@gmail.com>
2022-07-06 10:24:02 +0200

    refactor: Toast test before each usage

--- __tests__/Toast-test.tsx
+++ __tests__/Toast-test.tsx
@@ -9,23 +9,10 @@ import React from 'react';
 import {fireEvent, render, screen} from '@testing-library/react-native';
 import Toast from '../src/Toast/Toast';
 
-it('Toast renders correctly', () => {
-  render(
-    <Toast
-      title="Some test title"
-      visibilityState={[true, value => value]}
-      message="Some test message"
-    />,
-  );
+let handleClose: jest.Mock;
 
-  const testTitle = screen.getByText('Some test title');
-  const testMessage = screen.getByText('Some test message');
-  const testExitButton = screen.getByText('OK');
-  expect([testTitle, testMessage, testExitButton]).toBeDefined();
-});
-
-it('Toast closes correctly with button', () => {
-  const handleClose = jest.fn();
+beforeEach(() => {
+  handleClose = jest.fn();
 
   render(
     <Toast
@@ -34,7 +21,16 @@ it('Toast closes correctly with button', () => {
       message="Some test message"
     />,
   );
+});
 
+it('Toast renders correctly', () => {
+  const testTitle = screen.getByText('Some test title');
+  const testMessage = screen.getByText('Some test message');
+  const testExitButton = screen.getByText('OK');
+  expect([testTitle, testMessage, testExitButton]).toBeDefined();
+});
+
+it('Toast closes correctly with button', () => {
   const exitButton = screen.getByText('OK');
   fireEvent.press(exitButton);
 
